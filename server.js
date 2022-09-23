760a5abddbecd135ae608aeee787ce1e7a49243d
Siddarth R <siddarthroyalpally@gmail.com>
2022-09-22 23:17:54 -0400

    basic video call app

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+var https = require('https').Server(app);
+var io = require('socket.io')(https);
+
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('create or join', function (room) {
+        console.log('create or join to room ', room);
+        
+        var myRoom = io.sockets.adapter.rooms[room] || { length: 0 };
+        var numClients = myRoom.length;
+
+        console.log(room, ' has ', numClients, ' clients');
+
+        if (numClients == 0) {
+            socket.join(room);
+            socket.emit('created', room);
+        } else if (numClients == 1) {
+            socket.join(room);
+            socket.emit('joined', room);
+        } else {
+            socket.emit('full', room);
+        }
+    });
+
+    socket.on('ready', function (room){
+        socket.broadcast.to(room).emit('ready');
+    });
+
+    socket.on('candidate', function (event){
+        socket.broadcast.to(event.room).emit('candidate', event);
+    });
+
+    socket.on('offer', function(event){
+        socket.broadcast.to(event.room).emit('offer',event.sdp);
+    });
+
+    socket.on('answer', function(event){
+        socket.broadcast.to(event.room).emit('answer',event.sdp);
+    });
+
+});
+
+
+https.listen(port || 3000, function () {
+    console.log('listening on', port);
+});
