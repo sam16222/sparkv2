eca2d7f00851ea3b1402ab610eb4c9642ace5162
Jyothi Sumer <mjyothisumer@gmail.com>
2022-10-04 13:39:35 -0400

    Adding type module for test cases

--- test/test.js
+++ test/test.js
@@ -1,62 +1,51 @@
-// // with { "type": "module" } in your package.json
-// import { createServer } from "http";
-// import { io as Client } from "socket.io-client";
-// import { Server } from "socket.io";
-// import { assert } from "chai";
+// with { "type": "module" } in your package.json
+import { createServer } from "http";
+import { io as Client } from "socket.io-client";
+import { Server } from "socket.io";
+import { assert } from "chai";
 
-// // with { "type": "commonjs" } in your package.json
-// // const { createServer } = require("http");
-// // const { Server } = require("socket.io");
-// // const Client = require("socket.io-client");
-// // const assert = require("chai").assert;
+// with { "type": "commonjs" } in your package.json
+// const { createServer } = require("http");
+// const { Server } = require("socket.io");
+// const Client = require("socket.io-client");
+// const assert = require("chai").assert;
 
-// describe("my awesome project", () => {
-//     let io, serverSocket, clientSocket;
+describe("my awesome project", () => {
+    let io, serverSocket, clientSocket;
 
-//     before((done) => {
-//         const httpServer = createServer();
-//         io = new Server(httpServer);
-//         httpServer.listen(() => {
-//             const port = httpServer.address().port;
-//             clientSocket = new Client(`http://localhost:${port}`);
-//             io.on("connection", (socket) => {
-//                 serverSocket = socket;
-//             });
-//             clientSocket.on("connect", done);
-//         });
-//     });
+    before((done) => {
+        const httpServer = createServer();
+        io = new Server(httpServer);
+        httpServer.listen(() => {
+            const port = httpServer.address().port;
+            clientSocket = new Client(`http://localhost:${port}`);
+            io.on("connection", (socket) => {
+                serverSocket = socket;
+            });
+            clientSocket.on("connect", done);
+        });
+    });
 
-//     after(() => {
-//         io.close();
-//         clientSocket.close();
-//     });
+    after(() => {
+        io.close();
+        clientSocket.close();
+    });
 
-//     it("should work", (done) => {
-//         clientSocket.on("hello", (arg) => {
-//             assert.equal(arg, "world");
-//             done();
-//         });
-//         serverSocket.emit("hello", "world");
-//     });
+    it("should work", (done) => {
+        clientSocket.on("hello", (arg) => {
+            assert.equal(arg, "world");
+            done();
+        });
+        serverSocket.emit("hello", "world");
+    });
 
-//     it("should work (with ack)", (done) => {
-//         serverSocket.on("hi", (cb) => {
-//             cb("hola");
-//         });
-//         clientSocket.emit("hi", (arg) => {
-//             assert.equal(arg, "hola");
-//             done();
-//         });
-//     });
-// });
-
-
-
-var assert = require('assert');
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
+    it("should work (with ack)", (done) => {
+        serverSocket.on("hi", (cb) => {
+            cb("hola");
+        });
+        clientSocket.emit("hi", (arg) => {
+            assert.equal(arg, "hola");
+            done();
         });
     });
 });
