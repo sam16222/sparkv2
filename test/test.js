2c394f0ce1bf1fa627124fa28a39b93fc421881c
Jyothi Sumer <mjyothisumer@gmail.com>
2022-10-04 20:36:53 -0400

    Adding test case for checking HTTP request for client page

--- test/test.js
+++ test/test.js
@@ -1,3 +1,19 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { app, io } = require('../server.js');
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+
+describe("Spark", () => {
+    var iio = null
+    describe("HTTP request", () => {
+        it("Should send client page", (done) => {
+            chai.request(app).get('/').end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+        })
+    })
+})
