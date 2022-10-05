b1fcced66adfe9def95f09ff33ac2e049ce76ed4
Jyothi Sumer <mjyothisumer@gmail.com>
2022-10-05 18:44:09 -0400

    Opening sockets for test cases

--- test/test.js
+++ test/test.js
@@ -31,6 +31,21 @@ describe("Spark", () => {
 
     describe("Testing sockets", () => {
         var socket = null;
+        beforeEach(function (done) {
+            // Setup
+            socket = io.connect('http://localhost:3000', {
+                'reconnection delay': 0
+                , 'reopen delay': 0
+                , 'force new connection': true
+            });
+            socket.on('connect', function () {
+                console.log('worked...');
+                done();
+            });
+            socket.on('disconnect', function () {
+                console.log('disconnected...');
+            })
+        });
 
     })
 })
