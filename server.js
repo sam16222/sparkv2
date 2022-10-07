b2f25e009066c543d4d6d5b5979b654b1de03e48
aramasw <aramasw@ncsu.edu>
2022-10-06 20:30:07 -0400

    Added doc to src file

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var https = require('http').Server(app);
 var io = require('socket.io')(https);
 const path = require('path');
 
-// const options = {
+/**  const options = {
 //   key: fs.readFileSync('key.pem'),
 //   cert: fs.readFileSync('cert.pem')
-// };
+*/
 
 const port = process.env.PORT || 3000;
 
-// app.use(express.static('public'));
+/**  app.use(express.static('public'));*/
 app.use(express.static(path.join(__dirname, '/static')));
 
 app.get('/', function(req, res) {
@@ -23,10 +23,12 @@ app.get('/client.js', function (req, res) {
     res.sendFile(path.join(__dirname, 'client.js'));
   });
 
+  /** This is triggered when a client is connected */
 io.on('connection', function (socket) {
     console.log('a user connected');
     console.log('connect socket id:'  + `${socket.id}`);
 
+    /** This function is triggered when someone clicks to join the room */
     socket.on('create or join', function (room) {
         console.log('create or join to room ', room);
         
@@ -47,27 +49,33 @@ io.on('connection', function (socket) {
 
     });
 
+    /** This function is triggered when the person in the room is ready to communicate */
     socket.on('ready', function (room){
         socket.broadcast.to(room).emit('ready');
     });
 
+    /** This function is triggered when server gets a candidate from a person in the room */
     socket.on('candidate', function (event){
         socket.broadcast.to(event.room).emit('candidate', event);
     });
 
+    /** This function is triggered when server gets an offer from a person in the room */
     socket.on('offer', function(event){
         socket.broadcast.to(event.room).emit('offer',event.sdp);
     });
 
+    /** This function is triggered when server gets an answer from a person in the room */
     socket.on('answer', function(event){
         socket.broadcast.to(event.room).emit('answer',event.sdp);
     });
 
+    /** This function is triggered when a user disconnects */
     socket.on('disconnect', function () {
         console.log('a user disconnected');
     });
 });
 
+/** Starts the server */
 https.listen(port, function (error) {
     if(!error)
         console.log('listening on', port);
