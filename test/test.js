4b26bc907559a7190dc7c74e8ffcd83da783695a
Parth-25m <partha.25m@gmail.com>
2022-10-08 16:00:30 -0400

    [test]: added gesture tests
    * added test cases and sample input for gesture test
    * moved hand_gesture functions to hand_gesture.js

--- test/test.js
+++ test/test.js
@@ -2,10 +2,61 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { app } = require('../server.js');
 var io = require('socket.io-client');
+const hand_gesture = require('../hand_gesture')
+
 
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
+
+// Input class for gesture test
+class TestElement {
+    constructor(json_data) {
+        this.x = parseFloat(json_data["x"])
+        this.y = parseFloat(json_data["y"])
+        this.z = parseFloat(json_data["z"])
+    }
+}
+
+class TestEntries {
+    constructor(json_data) {
+        this.elem_arr = []
+        this.idx_arr = []
+        var idx = 0
+        for (var ele in json_data) {
+            this.elem_arr[idx] = new TestElement(json_data[ele])
+            this.idx_arr[idx] = idx
+            idx = idx + 1
+        }
+    }
+    
+    *entries() {
+        for (var i=0; i<this.elem_arr.length; i++) {
+            yield [i, this.elem_arr[i]];
+        }
+    }
+    [Symbol.iterator]() { return this.elem_arr.values() }
+}
+
+class TestGestureResults {
+    constructor(json_data) {
+    var entries = new TestEntries(json_data["multiHandLandmarks[0]"])
+    this.multiHandLandmarks = []//new MultiHandLandmarks(entries, 1)
+        this.multiHandLandmarks[0] = entries
+        this.lsit = []
+        for (var i in json_data["lsit"]) {
+            this.lsit.push(parseInt(json_data["lsit"][i]))
+        }
+    }
+}
+
+function json_to_obj(json_arr) {
+    var TestResults = []
+    for (var key in json_arr) {
+        TestResults.push(new TestGestureResults(json_arr[key]))
+    }
+    return TestResults
+}
 
 describe("Spark", () => {
     describe("HTTP request", () => {
@@ -73,6 +124,36 @@ describe("Spark", () => {
                 done();
             });
         });
+    })
+
+    describe("test gesture", () => {
+        it("Should match pre-calculated results", (done) => {
+            var PreCalculatedRes = {}
+            PreCalculatedRes[hand_gesture.Gesture.All5Fingers] = 228;
+            PreCalculatedRes[hand_gesture.Gesture.NoDetection] = 74;
+            PreCalculatedRes[hand_gesture.Gesture.ThumbsUp] = 180;
+            PreCalculatedRes[hand_gesture.Gesture.ThumbsDown] = 113;
+            
+            var fs = require('fs');
+            var obj = JSON.parse(fs.readFileSync('./gestureTestData', 'utf8'));
+            var TestResults = json_to_obj(obj)
+            var res = {}
+            for (var idx in TestResults) {
+                var key = hand_gesture.onResults(TestResults[idx])
+                if (res[key] === undefined) {
+                    res[key] = 0
+                }
+                res[key]++
+            }
+
+            if ((PreCalculatedRes[hand_gesture.Gesture.All5Fingers] === res[hand_gesture.Gesture.All5Fingers]) &&
+                    (PreCalculatedRes[hand_gesture.Gesture.NoDetection] === res[hand_gesture.Gesture.NoDetection]) && 
+                    (PreCalculatedRes[hand_gesture.Gesture.ThumbsUp] === res[hand_gesture.Gesture.ThumbsUp]) && 
+                    (PreCalculatedRes[hand_gesture.Gesture.ThumbsDown] === res[hand_gesture.Gesture.ThumbsDown])) {
+                done()
+            }
+
+        })
     })
 
     describe("Closing spark server", () => {
