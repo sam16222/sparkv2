e0af464c97f516e01ef2188f40a18e2dcddbab4c
Jyothi Sumer <mjyothisumer@gmail.com>
2022-10-08 14:38:26 -0400

    Adding test case for root page

--- test/test.js
+++ test/test.js
@@ -6,3 +6,14 @@ var io = require('socket.io-client');
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
+
+describe("Spark", () => {
+    describe("HTTP request", () => {
+        it("Should send client page", (done) => {
+            chai.request(app).get('/').end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+        })
+    })
+})
