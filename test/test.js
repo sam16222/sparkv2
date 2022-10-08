bd269bbd8a13424cebfdcb2f6b6d4a6e4eb115b9
Jyothi Sumer <mjyothisumer@gmail.com>
2022-10-08 14:42:22 -0400

    Testing screen share

--- test/test.js
+++ test/test.js
@@ -21,5 +21,11 @@ describe("Spark", () => {
                 done();
             })
         })
+        it("Should receive screen-sharing-min.html file", (done) => {
+            chai.request(app).get('/share').end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+        })
     })
 })
