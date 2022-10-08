5d02d4334f6f1451402f3d3096ec712b6cd4f71c
Jyothi Sumer <mjyothisumer@gmail.com>
2022-10-08 14:34:21 -0400

    Updating chai configuration

--- test/test.js
+++ test/test.js
@@ -2,3 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { app } = require('../server.js');
 var io = require('socket.io-client');
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
