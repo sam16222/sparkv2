dfac3b72db7b396f49c0863506ac9e9bcafeb568
Natalie Kraft <nakraft@ncsu.edu>
2022-10-09 14:11:35 -0400

    Adding in in-software documentation

---
+++ scripts/help.py
@@ -0,0 +1,18 @@
+print("Welcome to Spark!")
+print("The best hands free web conferencing software in Raleigh, NC.")
+print()
+
+print("-------Help Guide-------")
+print()
+print("Here are some places where to start:")
+
+print(" - First, try to install needed dependencies with \"npm install\".")
+print(" - Then, see about running your test cases with \"npm run test\".")
+print(" - Now you should be able to launch the app with \"npm run start\" or \"nodemon server.js\".")
+print()
+
+print("Just visit your localhost:3000 to find the application and start sharing!")
+print()
+print("--------------")
+print()
+print("Visit our online documentation (https://github.com/SiddarthR56/spark/blob/main/README.md) for more support.")
